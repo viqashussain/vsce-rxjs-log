a43a5067e7198b61bdd2c937232d2b139e619293
viqashussain <viqashussain@hotmail.co.uk>
2019-07-25 18:27:10 +0100

    update readme and remove comments.

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,7 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rxjs-subscribe-cl" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.rxjs-subscribe-cl', () => {
-		// The code you place here will be executed every time your command is executed
 
 		let editor = vscode.window.activeTextEditor;
         if (!editor) {
@@ -27,13 +14,9 @@ export function activate(context: vscode.ExtensionContext) {
 		const newText = `${text}.subscribe(x => console.log(x));`;
 
 		editor.edit(x => x.replace(selection, newText));
-
-
-		vscode.window.showInformationMessage(text);
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
